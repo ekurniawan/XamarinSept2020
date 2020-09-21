c2b0eef35e0528c18512e601faccb55d2d489e68
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-09-21 11:20:38 +0700

    Add project files.

---
+++ ContohXamarinSep2020/ContohXamarinSep2020/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContohXamarinSep2020
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new ContohAbsoluteLayout();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
