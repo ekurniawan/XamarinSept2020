9a1d7ae714c39df0bd069938555e61c0faf87dd4
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-09-23 11:09:44 +0700

    menambahkan delete data

---
+++ ContohXamarinSep2020/ContohXamarinSep2020/EditEmployeeSQL.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ContohXamarinSep2020.DAL;
+using ContohXamarinSep2020.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContohXamarinSep2020
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditEmployeeSQL : ContentPage
+    {
+        private EmployeeDAL _empDAL;
+        public EditEmployeeSQL()
+        {
+            InitializeComponent();
+            _empDAL = new EmployeeDAL();
+        }
+
+        private async void btnEdit_Clicked(object sender, EventArgs e)
+        {
+            var editData = new Employee
+            {
+                EmpId = Convert.ToInt32(txtID.Text),
+                EmpName = txtEmpName.Text,
+                Department = txtDepartment.Text,
+                Designation = txtDesignation.Text,
+                Qualification = txtQualification.Text
+            };
+
+            try
+            {
+                _empDAL.Edit(editData);
+                await DisplayAlert("Keterangan", "Edit Data Berhasil", "OK");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"{ex.Message}", "OK");
+            }
+        }
+    }
+}
--- ContohXamarinSep2020/ContohXamarinSep2020/ShowEmployee.xaml.cs
+++ ContohXamarinSep2020/ContohXamarinSep2020/ShowEmployee.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ContohXamarinSep2020.DAL;
+using ContohXamarinSep2020.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,17 +21,53 @@ namespace ContohXamarinSep2020
             _empDAL = new EmployeeDAL();
         }
 
+        private void RefreshData()
+        {
+            var results = _empDAL.GetAll();
+            lvEmployee.ItemsSource = results;
+        }
+
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
-            var results = _empDAL.GetAll();
-            lvEmployee.ItemsSource = results;
+            RefreshData();
         }
 
         private async void btnAdd_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new AddEmployeeSQL());
+        }
+
+        private async void lvEmployee_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var data = (Employee)e.Item;
+            var formEdit = new EditEmployeeSQL();
+            formEdit.BindingContext = data;
+            await Navigation.PushAsync(formEdit);
+        }
+
+        private async void btnDelete_Clicked(object sender, EventArgs e)
+        {
+            var myBtn = (Button)sender;
+            var delEmp = new Employee
+            {
+                EmpId = Convert.ToInt32(myBtn.CommandParameter)
+            };
+            try
+            {
+                var answer = await DisplayAlert("Konfirmasi", "Apakah anda yakin untuk delete data?", "Yes", "No");
+                if (answer)
+                {
+                    _empDAL.Delete(delEmp);
+                    await DisplayAlert("Keterangan", $"ID: {myBtn.CommandParameter} berhasil di delete", "OK");
+                    RefreshData();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"{ex.Message}", "OK");
+            }
+           
         }
     }
 }
